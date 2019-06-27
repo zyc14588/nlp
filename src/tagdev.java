e8ee4dc14025895816e9b22f82ad67624d2c8570
zyc14588 <496000076@qq.com.com>
2019-06-27 16:37:55 +0800

    增量更新

---
+++ src/tagdev.java
@@ -0,0 +1,13 @@
+public class tagdev {
+    private hero[] hh;
+    private tag[] tg;
+    private page[]pg;
+    private pagehot[] pgh;
+    tagdev(hero[] h, tag[] t, page[]p, pagehot[]ph){
+        hh=h;
+        tg=t;
+        pg=p;
+        pgh=ph;
+    }
+
+}
